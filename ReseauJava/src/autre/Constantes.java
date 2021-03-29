3b1119edd6aefe586c8a7395501d1a893551cf98
AlexMabea <60853509+AlexMabea@users.noreply.github.com>
2021-03-29 11:57:46 +0200

    Add files via upload

---
+++ ReseauJava/src/autre/Constantes.java
@@ -0,0 +1,19 @@
+package autre;
+/*
+ * Classe contenant les constantes de connexion
+ */
+public class Constantes {
+	public static String SERVER_HOST = "localhost";
+	public static int SERVER_PORT = 7777;
+	public static int SERVER_MAX_CLIENT = 10;
+	
+	public static String SEP = ";";
+	
+	public static String LOGS = "server.log";
+	
+	public static String DB_HOST = "localhost";
+	public static String DB_NAME = "projet";
+	public static String DB_PORT = "5432";
+	public static String DB_USER = "projet";
+	public static String DB_PWD = "pwdprojet";
+}
