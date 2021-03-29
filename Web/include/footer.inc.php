75cf747583bcccf376924e335728a5c0d8bed7c9
AlexMabea <60853509+AlexMabea@users.noreply.github.com>
2021-03-29 11:47:59 +0200

    Add files via upload

---
+++ Web/include/footer.inc.php
@@ -0,0 +1,10 @@
+		<footer> 
+		<ul>
+			<li> Projet Base de donnée - Réseau </li>
+			<li> Mabea Hans </li>
+			<li> Mathis Teixeira </li>
+			<li> Licence Informatique 3e Année CY </li>
+		</ul>
+	</footer>
+</body>
+</html>
---
+++ Web/include/header.inc.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title> <?php echo $title ?> </title>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Mabea Hans"/>
+		<meta name="author" content="Mathis Teixeira"/>
+		<meta name="date" content="2020-11-20"/>
+		<link rel="stylesheet" type="text/css" href="styleprojet.css"/>
+	</head>
+<body>
+
+<header>
+	<nav class="menu-navigation">
+		<ul>
+	
+			<li>
+				<a> Liberama votre plateforme numérique </a>
+			</li>
+			
+			<li class="bouton">
+				<a href="pageprincipale.php"> Bibliothèque </a> 
+			</li>
+			
+			<li class="bouton"> 
+				<a href="commande.php">  Mes Commandes </a> 
+			</li>
+			
+			<li class="bouton"> 
+			    <a href='panier.php?'> Panier </a>
+			</li>
+			
+			<li class="boutondeco">
+				<a href="deconnexion.php"> Deconnexion </a>
+			</li>
+
+		</ul>
+	</nav>
+</header>
